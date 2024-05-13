0b137cbc3a1bd188c98a629ca3632f09b291ab12
DylanRuth <d2ruth@uwaterloo.ca>
2024-05-13 18:24:36 -0400

    Use proper imports for streamlit

--- Talkwithpdf.py
+++ Talkwithpdf.py
@@ -1,7 +1,7 @@
-# __import__("pysqlite3")
-# import sys
+__import__("pysqlite3")
+import sys
 
-# sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 import os
 import base64
 from langchain_community.document_loaders import PyPDFLoader
